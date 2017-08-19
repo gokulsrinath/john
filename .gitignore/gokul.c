dfdee21f8a6f87eb547e169287d974820cefc914
gokulsrinath <30368312+gokulsrinath@users.noreply.github.com>
2017-08-19 16:40:12 +0530

    Update gokul.c

--- .gitignore/gokul.c
+++ .gitignore/gokul.c
@@ -8,7 +8,7 @@ int main()
  
     // True if the number is perfectly divisible by 2
     if(number % 2 == 0)
-        printf("%d is even.", number);
+        printf("%d IS EVEN.", number);
     else
         printf("%d is odd.", number);
  
